ce713500048b1ff79701147a4abffe8af39bd1de
Fromant <framanchannel@gmail.com>
2025-10-21 15:09:20 +0300

    loops practice 2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int i = 0;
+
+        int j = 0;
+
+        for (; i < 10; i++) {
+            if (i == 5) break;
+            if (i < 3) continue; //skip increment
+            j++; //j will only be incremented 3 times (i=0, 1, 2)
+        }
+
+        System.out.println(i); // i = 5
+        System.out.println(j); // j = 2
     }
 }
